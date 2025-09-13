dd945a7ddeb1a94b3fb76ee87669022b416151e6
finit-8 <pwg0223@gmail.com>
2025-09-13 23:59:17 +0900

    클래스 분리해서 알고리즘 연습

--- repeat/src/repeat/ClassTask4.java
+++ repeat/src/repeat/ClassTask4.java
@@ -1,15 +1,23 @@
+package repeat;
+public class ClassTask4 {
 // 1. 2개의 값을 전달하면 최솟값과 최댓값을 구해주는 메서드
+	public void getMaxAndMin(int num1, int num2) {
+		int result = 0, max = 0, min = 0;
+		if(num1 > num2) {
+			max = num1;
+//			return max;
+		} else if(num1 < num2) {
+			min = num1;
+//			return min;
+		}
+	}
 // 2. 클래스로 리턴
 // 3. 메인 메서드에서 메서드 사용
-package repeat;
-
-
-
-public class ClassTask4 {
-   
-   
-   public static void main(String[] args) {
-      
-   }
+	
+	public static void main(String[] args) {
+		ClassTask4 ct = new ClassTask4();
+//		System.out.println(ct.getMaxAndMin(3, 9));
+		ct.getMaxAndMin(3, 9);
+	}
    
 }
